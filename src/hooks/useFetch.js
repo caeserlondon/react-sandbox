777664823177ca78cfeaadd2ca7c5e7e2f82535c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-17 21:18:19 +0100

    Creating useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+const useFetch = (url, options) => {
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+	const [data, setData] = useState(null)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(url, options)
+				const data = await response.json()
+
+				setData(data)
+				setLoading(false)
+			} catch (error) {
+				setError(error)
+				setLoading(false)
+			}
+		}
+		fetchData()
+		// eslint-disable-next-line
+	}, [])
+
+	return { loading, error, data }
+}
+
+export default useFetch
