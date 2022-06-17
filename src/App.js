3b9a66553434bb10156fe269e44767dbd98be409
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-17 21:21:13 +0100

    Adding the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+// import UseRefExample1 from './components/UseRefExample1'
+// import UseRefExample2 from './components/UseRefExample2'
+// import UseRefExample3 from './components/UseRefExample3'
+// import UseMemoExample from './components/UseMemoExample'
+// import UseCallbackExample from './components/UseCallbackExample'
+// import CustomHookExample1 from './components/CustomHookExample1'
+import CustomHookExample2 from './components/CustomHookExample2'
+
 function App() {
 	return (
 		<div className='container mt-5'>
-			<h1>Hello Sandbox</h1>
+			{/* <UseRefExample1 /> */}
+
+			{/* <UseRefExample2 /> */}
+
+			{/* <UseRefExample3 /> */}
+
+			{/* <UseMemoExample /> */}
+
+			{/* <UseCallbackExample /> */}
+
+			{/* <CustomHookExample1 /> */}
+
+			<CustomHookExample2 />
 		</div>
 	)
 }
